3db46fc7a8a294587a3c92cb8bfeeaa74937011a
unknown <brownnguyen268@gmail.com>
2020-08-04 14:16:35 +0700

    add

--- js/index.js
+++ js/index.js
@@ -1,17 +1,28 @@
 var header = document.querySelector(".header");
 var contactTop = document.querySelector(".ct-scrl-top .contact");
 var scroll = document.querySelector('.ct-scrl-top .scroll');
+var burgerHead = document.querySelector('.burger-head');
+var headMenu = document.querySelector('.head-menu');
 window.onscroll = function () {
     let pageY = window.pageYOffset;
-    if (pageY > 300) {
+    if (pageY > 600) {
         contactTop.classList.add('stick');
         contactTop.style.height = "75px";
         header.style.height = "75px";
     }
     else {
         contactTop.classList.remove('stick');
-        contactTop.style.height = "190px";
-        header.style.height = "110px";
+        contactTop.style.height = "";
+        header.style.height = "";
+    }
+}
+burgerHead.onclick = function () {
+    burgerHead.classList.toggle('expand');
+    if (burgerHead.classList.contains('expand')) {
+        headMenu.classList.add('expand')
+    }
+    else {
+        headMenu.classList.remove('expand')
     }
 }
 $("#scroll").click(function () {
@@ -19,3 +30,9 @@ $("#scroll").click(function () {
         scrollTop: $("#aim").offset().top
     }, 1500);
 });
+var scroll = document.querySelector('#scroll');
+var aim = document.querySelector('#aim');
+scroll.onclick = function(){
+    var aimTo = aim.scrollTop;
+    console.log(aimTo)
+}
